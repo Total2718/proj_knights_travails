ad44e6aa12b66f681e74677be499ba42fa2f45ac
Total2718 <jlacenere@outlook.com>
2021-09-07 07:41:01 -0400

    The knight successfully finds the shortest path via looking to it's parent until there is no parent to look to.

--- main.rb
+++ main.rb
@@ -2,30 +2,28 @@
 
 
 class KnightPath
+    attr_accessor :position, :parent
 
     TRANSFORMATIONS = [[1,2], [2,1], [-1,2], [2,-1],
                          [-2,1], [-1,-2], [-2,-1], [1,-2]].freeze
     @@history = []
 
-    def initialize(position, parent = nil )
+    def initialize(position, parent)
         @parent = parent
         @position = position
         @@history.push(position)
     end
 
-    def children()
+    def children
        pos_mov = TRANSFORMATIONS.map do |transf|
         x = @position[0] + transf[0]
         y = @position[1] + transf[1]
-            if (x >= 0 && x <= 7) && (y >= 0 && y <= 7)
                 new_spot = [x, y]
-            end
        end
-       new_spot.map do |spot|
-        if @@history.include? (spot) == false
-            KnightPath.new(spot, @position)
-        end
-       end
+       pos_mov.keep_if{|spot| spot[0].between?(0, 7) && spot[1].between?(0, 7)}
+       m = pos_mov.reject { |e| @@history.include?(e) }
+       m.map {|e| KnightPath.new(e, self)}
+
        
     end
 
@@ -33,9 +31,25 @@ end
 
 def knight_moves(start_pos, end_pos)
     queue = []
-    current_node = KnightPath.new(start_pos)
+    current_node = KnightPath.new(start_pos, nil)
     until current_node.position == end_pos
-        current_node.children.each {|child| queue.push(child)}
+        current_node.children.each do |child| 
+            queue.push(child)
+            
+        end
+        current_node = queue.shift
+    end
+    identify_path(current_node)
 end
 
+def identify_path(node)
+    
+    identify_path(node.parent) unless node.parent.nil?
+    
+    print node.position[0]
+    print " "
+    puts node.position[1]
 
+end
+
+knight_moves([4,5], [5,5])
